4c2fa86fcacd3d864da5f901ed6b55a733fad69e
locnguyen3108 <loclieulinh318@gmail.com>
2024-03-05 13:28:24 +0700

    lab3

---
+++ lab3/demo/src/main/java/com/example/demo/service/CourseService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.model.Course;
+import org.springframework.data.domain.Page;
+
+public interface CourseService {
+    List<Course> getAllCourses();
+    void saveCourse(Course course);
+    Course getCourseById(long id);
+    void deleteCourseById(long id);
+    Page<Course> findPaginated(int pageNum, int pageSize,
+                               String sortField,
+                               String sortDirection);
+}
