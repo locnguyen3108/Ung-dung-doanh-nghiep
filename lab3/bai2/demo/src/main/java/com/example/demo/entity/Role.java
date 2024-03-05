8b8a3380e5864b501ee3f9d070382c7f2a35b9c8
locnguyen3108 <loclieulinh318@gmail.com>
2024-03-05 15:50:01 +0700

    bai2 done

---
+++ lab3/bai2/demo/src/main/java/com/example/demo/entity/Role.java
@@ -0,0 +1,22 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+
+@Entity
+@Data
+@Table(name = "roles")
+public class Role {
+    @Id
+    @Column(name = "role_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    public Integer getId() {
+        return id;
+    }
+
+    // remaining getters and setters
+}
---
+++ lab3/bai2/demo/src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+import com.example.demo.entity.Role;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.*;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private boolean enabled;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    // remaining getters and setters are not shown for brevity
+}
